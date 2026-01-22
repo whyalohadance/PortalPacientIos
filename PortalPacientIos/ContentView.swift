dfe7fd0e74c35583a6d2cc7792fdb9e5e53f87ee
whyalohadance <lpline51@gmail.com>
2026-01-22 09:54:52 +0200

    Initial Commit

---
+++ PortalPacientIos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PortalPacientIos
+//
+//  Created by devid ceban on 22.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PortalPacientIos/PortalPacientIosApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortalPacientIosApp.swift
+//  PortalPacientIos
+//
+//  Created by devid ceban on 22.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct PortalPacientIosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
